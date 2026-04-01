73f0a2390d9539de17b9697241c285d4444f0859
teairasaurusrex <terenceruis@proton.me>
2026-04-01 13:08:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Vercel expects 'dist'
+  esbuild: {
+    tsconfig: './tsconfig.json', // Force Vite to use this tsconfig
   },
-})
+  build: {
+    outDir: 'dist', // optional, default is 'dist'
+    sourcemap: false, // optional, can enable if needed
+  },
+});
