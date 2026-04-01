8a7b4b8f3f4f9728bc49043796a57e94897efcae
teairasaurusrex <terenceruis@proton.me>
2026-04-01 13:01:36 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import WorldCupDashboard from './App'
+import App from './App'
 import './index.css'
 
-const rootElement = document.getElementById('root');
-
+const rootElement = document.getElementById('root')
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <WorldCupDashboard />
+      <App />
     </React.StrictMode>
   )
 }
